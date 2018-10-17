e800998cbaa83dbcf0e301fb9123512c79fe4d2e
Martin Varga <martin.varga@lutraconsulting.co.uk>
2018-10-17 11:17:09 +0200

    Added HN RAMI support.

---
+++ scripts/test_HN.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+import psycopg2 as pg
+
+def get_table_columns(connection, schema, table):
+    cols = []
+    try:
+        cursor = connection.cursor()
+        sql = "SELECT * FROM {}.{} LIMIT 0".format(schema, table)
+        cursor.execute(sql)
+        for desc in cursor.description:
+            cols.append(desc[0])
+        cursor.close()
+    except pg.Error as e:
+        print e
+    return cols
+
+def check_tables():
+    connection = pg.connect('dbname=ostII user=postgres password=postgres host=localhost port=5432')
+    schema = 'test'
+    # for Roads
+    #tables = ['ferrynode', 'ferryterminal', 'ferrylink', 'road', 'roadjunction', 'roadlink', 'roadnode', 'street']
+    # for RAMI
+    #tables = ['accessrestriction', 'hazard', 'highwaydedication', 'reinstatement', 'restrictionforvehicles', 'specialdesignation', 'structure', 'turnrestriction']
+    tables = ['structure', 'turnrestriction']
+    for t in tables:
+        columns = get_table_columns(connection, schema, t)
+        for c in columns:
+            try:
+                cur = connection.cursor()
+                sql = "SELECT DISTINCT {} FROM {}.{} LIMIT 2".format(c, schema, t)
+                cur.execute(sql)
+                results = cur.fetchall()
+                if results[0][0] is None:
+                    print t + ": " + c + " ...INVALID"
+                else:
+                    print t + ": " + c + " ...OK"
+                cur.close()
+            except pg.Error as e:
+                print e
+
+if __name__ == '__main__':
+    check_tables()
