f652bc3d0256fd9351cd7ef7c55c15d3cad78db5
Peter Wells <peter.wells@lutraconsulting.co.uk>
2018-12-07 14:47:22 +0000

    Added HN Paths Merged Roads and RAMI (HN) Bug fix for testing script

--- scripts/test_HN.py
+++ scripts/test_HN.py
@@ -15,19 +15,20 @@ def get_table_columns(connection, schema, table):
     return cols
 
 def check_tables():
-    connection = pg.connect('dbname=ostII user=postgres password=postgres host=localhost port=5432')
-    schema = 'test'
+    connection = pg.connect('dbname=training user=postgres password=postgres host=localhost port=5432')
+    schema = 'hn_paths'
     # for Roads
     #tables = ['ferrynode', 'ferryterminal', 'ferrylink', 'road', 'roadjunction', 'roadlink', 'roadnode', 'street']
     # for RAMI
     #tables = ['accessrestriction', 'hazard', 'highwaydedication', 'reinstatement', 'restrictionforvehicles', 'specialdesignation', 'structure', 'turnrestriction']
-    tables = ['structure', 'turnrestriction']
+    # tables = ['connectinglink', 'connectingnode', 'ferrylink', 'ferrynode', 'ferryterminal', 'highwaydedication', 'maintenance', 'path', 'pathlink', 'pathnode', 'reinstatement', 'specialdesignation', 'street']
+    tables = ['street']
     for t in tables:
         columns = get_table_columns(connection, schema, t)
         for c in columns:
             try:
                 cur = connection.cursor()
-                sql = "SELECT DISTINCT {} FROM {}.{} LIMIT 2".format(c, schema, t)
+                sql = "SELECT DISTINCT {} FROM {}.{} ORDER BY {} NULLS LAST LIMIT 2".format(c, schema, t, c)
                 cur.execute(sql)
                 results = cur.fetchall()
                 if results[0][0] is None:
